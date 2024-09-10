e6df38068dea033624b79f6dc98453088d5b7a53
Brandon Davis <git@subdavis.com>
2024-09-09 22:05:01 -0500

    Hugo yaml

--- lims_utils/scripts.py
+++ lims_utils/scripts.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from time import sleep
 
 from lims_utils.claude import generate_summary
-from lims_utils.meetings import update_meeting_cache
+from lims_utils.meetings import update_meeting_cache, load_meeting_cache
 from lims_utils.transcript import formatSummaryForHugo
 load_dotenv()
 
@@ -13,13 +13,13 @@ def updateCache():
 
 
 def generate():
-    cachedMeetings, newMeetings = update_meeting_cache("2024-01-01")
+    cacheList = load_meeting_cache()
     # Get cached meetings sorted by date
     cachedMeetings = sorted(
-        cachedMeetings, key=lambda x: date_parse(x["meetingTime"]), reverse=True
+        cacheList, key=lambda x: date_parse(x["meetingTime"]), reverse=True
     )
 
-    for meeting in cachedMeetings[:8]:
+    for meeting in (cachedMeetings[:8]):
         filename, text, isNewContent = generate_summary(meeting)
         formatSummaryForHugo(meeting, text)
         if isNewContent:
